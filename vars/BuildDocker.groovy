fff1616641a0cd8c0dbfcd3b110f48d215f2c5c1
Mohamed Raiyan <mohamed_raiyan@epam.com>
2023-08-28 22:03:02 +0530

    rename tag

--- vars/BuildDocker.groovy
+++ vars/BuildDocker.groovy
@@ -1,6 +1,6 @@
 def call(DockerHubUser, AppName, dockerImageTag) {
     sh """
     docker build -t $DockerHubUser/$AppName:v$dockerImageTag .
-    docker tag $DockerHubUser/$AppName:$dockerImageTag $DockerHubUser/$AppName:latest
+    docker tag $DockerHubUser/$AppName:v$dockerImageTag $DockerHubUser/$AppName:latest
     """
 }

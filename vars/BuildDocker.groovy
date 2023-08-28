e579e8ea53411e5e618cedbeef63ff2b9203dc65
Mohamed Raiyan <mohamed_raiyan@epam.com>
2023-08-28 20:50:39 +0530

    updated BuildDocker script

--- vars/BuildDocker.groovy
+++ vars/BuildDocker.groovy
@@ -1,6 +1,6 @@
 def call(DockerHubUser, AppName, dockerImageTag) {
     sh """
     docker build -t $DockerHubUser/$AppName:$dockerImageTag .
-    docker tag -t $DockerHubUser/$AppName:$dockerImageTag $DockerHubUser/$AppName:latest
+    docker tag $DockerHubUser/$AppName:$dockerImageTag $DockerHubUser/$AppName:latest
     """
 }

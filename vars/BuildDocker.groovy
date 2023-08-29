5538445c3ce6c2ed61a27e704a0f85acb6c39609
Mohamed Raiyan <mohamed_raiyan@epam.com>
2023-08-29 13:32:08 +0530

    updated BuildDocker script for ECR

--- vars/BuildDocker.groovy
+++ vars/BuildDocker.groovy
@@ -1,6 +1,13 @@
-def call(DockerHubUser, AppName, dockerImageTag) {
+// def call(DockerHubUser, AppName, dockerImageTag) {
+//     sh """
+//     docker build -t $DockerHubUser/$AppName:v$dockerImageTag .
+//     docker tag $DockerHubUser/$AppName:v$dockerImageTag $DockerHubUser/$AppName:latest
+//     """
+// }
+
+def call(aws_account_id, ecr-repoName, region) {
     sh """
-    docker build -t $DockerHubUser/$AppName:v$dockerImageTag .
-    docker tag $DockerHubUser/$AppName:v$dockerImageTag $DockerHubUser/$AppName:latest
+    docker build -t ${ecr-repoName}:${dockerImageTag} .
+    docker tag ${ecr-repoName}:${dockerImageTag} ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr-repoName}:${dockerImageTag}
     """
 }

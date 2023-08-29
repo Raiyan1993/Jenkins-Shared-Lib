0cc708f06e1d90f4896499f1747cb2b9373c3eb6
Mohamed Raiyan <mohamed_raiyan@epam.com>
2023-08-29 14:35:02 +0530

    Updated groovy script for ECR

--- vars/DockerImagePush.groovy
+++ vars/DockerImagePush.groovy
@@ -1,14 +1,21 @@
-def call(DockerHubUser, AppName, dockerImageTag) {
-    withCredentials([usernamePassword(
-        credentialsId: 'docker-creds', 
-        passwordVariable: 'PASS', 
-        usernameVariable: 'USER'
-        )]) {
+// def call(DockerHubUser, AppName, dockerImageTag) {
+//     withCredentials([usernamePassword(
+//         credentialsId: 'docker-creds', 
+//         passwordVariable: 'PASS', 
+//         usernameVariable: 'USER'
+//         )]) {
 
-        sh """
-        docker login -u '$USER' -p '$PASS'
-        docker push $DockerHubUser/$AppName:v$dockerImageTag
-        docker push $DockerHubUser/$AppName:latest
-        """
-        }
+//         sh """
+//         docker login -u '$USER' -p '$PASS'
+//         docker push $DockerHubUser/$AppName:v$dockerImageTag
+//         docker push $DockerHubUser/$AppName:latest
+//         """
+//         }
+// }
+
+def call(aws_account_id, repoName, dockerImageTag, region) {
+    sh """
+    aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+    docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${repoName}:${dockerImageTag}
+    """
 }
--- vars/DockerImageScan.groovy
+++ vars/DockerImageScan.groovy
@@ -1,6 +1,13 @@
-def call(DockerHubUser, AppName, dockerImageTag) {
+// def call(DockerHubUser, AppName, dockerImageTag) {
+//     sh """
+//     trivy image $DockerHubUser/$AppName:v$dockerImageTag > scan.txt
+//     cat scan.txt
+//     """
+// }
+
+def call(aws_account_id, repoName, dockerImageTag, region) {
     sh """
-    trivy image $DockerHubUser/$AppName:v$dockerImageTag > scan.txt
+    trivy ${repoName}:${dockerImageTag} ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${repoName}:${dockerImageTag} > scan.txt
     cat scan.txt
     """
 }

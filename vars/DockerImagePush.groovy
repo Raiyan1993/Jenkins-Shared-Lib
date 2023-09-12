a03fc5894f8114743dddcda5abd92ae70f923670
Mohamed Raiyan <mohamed_raiyan@epam.com>
2023-09-12 19:41:48 +0530

    creating ECR repo on the move

--- vars/DockerImagePush.groovy
+++ vars/DockerImagePush.groovy
@@ -19,6 +19,7 @@ def call(aws_account_id, repoName, dockerImageTag, region) {
 
         sh """
         aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+        aws ecr create-repository --repository-name ${repoName} --image-scanning-configuration scanOnPush=true
         docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${repoName}:${dockerImageTag}
         docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${repoName}
         """                   
